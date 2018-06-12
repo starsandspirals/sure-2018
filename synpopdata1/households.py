1f7e20ebc9f25449aaca31e720bedf73c7536d9f
Daniel Marshall <daniel@starsandspirals.com>
2018-06-12 11:29:51 +0100

    Fixed index bug, created column labels for output

--- synpopdata1/households.py
+++ synpopdata1/households.py
@@ -5,12 +5,21 @@ data = pandas.read_csv('histo.csv')
 sexage = data['sexage']
 colsums = data.sum(axis=0)[1:]
 
-household_sizes = list(map (int, colsums.index.values))
-households = colsums.divide(household_sizes)
-households = pandas.Series(households.tolist(), dtype=int)
+household_sizes = list (map (int, colsums.index.values))
+households = colsums.divide (household_sizes)
+households = households.astype(int)
 
 rowsums = data.sum(axis=1)
-people = pandas.Series(rowsums.tolist(), index=sexage)
+people = pandas.Series (rowsums.tolist(), index=sexage)
 
+col_labels = []
+
+for i, v in households.items():
+  
+  for y in range (1, v):
+
+    label = str(y) + ", H" + str(i)
+    col_labels.append(label)
+
+print(col_labels)
 print(households)
-print(people)
