3be59bea8d827131bd2760a6137f75f53c338010
Daniel Marshall <daniel@starsandspirals.com>
2018-06-26 11:10:29 +0100

    Only write number of sizes once

--- Project/src/model/preprocess.py
+++ Project/src/model/preprocess.py
@@ -2,9 +2,11 @@ import pandas
 
 data = pandas.read_csv('histo.csv')
 fileout = open('data.in', 'w')
+sizes = len(data.columns) - 1
+
+fileout.write(str(sizes) + '\n')
 
 for index, row in data.iterrows():
-  count = 0
   indices, columns = [], []
   category = row['sexage'].split(':')
   gender, agegroup = category[0], category[1].split(',')
@@ -14,16 +16,11 @@ for index, row in data.iterrows():
   for index, col in row[1:].iteritems():
     indices.append(index)
     columns.append(col)
-    count += 1
-
-  fileout.write(str(count) + '\n')
 
   for n in range(1, len(indices)):
     fileout.write(indices[n-1] + '\n')
     fileout.write(str(columns[n-1]) + '\n')
   
-
-
-  
+fileout.close()
   
 
